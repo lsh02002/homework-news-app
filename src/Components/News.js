7ff981e9ecf012f79262f873fd6214511c21ce12
lsh02002 <40713089+lsh02002@users.noreply.github.com>
2023-11-25 21:47:20 +0900

    Add files via upload

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,7 +2,7 @@ import { useContext, useState } from "react";
 import { NewsContext } from "../Context/NewsContext";
 import { NewsContent } from "../Components/NewsContent";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+//import "react-calendar/dist/Calendar.css";
 import "./News.css";
 
 const News = () => {
